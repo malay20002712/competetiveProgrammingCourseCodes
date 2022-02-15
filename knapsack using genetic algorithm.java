7f4a0f2e8e03031b18733f36afda15f87f4edb5a
malay20002712 <57236934+malay20002712@users.noreply.github.com>
2022-02-15 14:43:14 +0530

    Update knapsack using genetic algorithm.java

--- knapsack using genetic algorithm.java
+++ knapsack using genetic algorithm.java
@@ -5,56 +5,7 @@ public class Main {
     Individual fittest;
     Individual secondFittest;
     int generationCount = 0;
-
-    public static void main(String[] args) {
-
-        Random rn = new Random();
-
-        Main demo = new Main ();
-
-        //Initialize population
-        demo.population.initializePopulation(10);
-
-        //Calculate fitness of each individual
-        demo.population.calculateFitness();
-
-        System.out.println("Generation: " + demo.generationCount + " Fittest: " + demo.population.fittest);
-
-        //While population gets an individual with maximum fitness
-        while (demo.population.fittest < 5) {
-            ++demo.generationCount;
-
-            //Do selection
-            demo.selection();
-
-            //Do crossover
-            demo.crossover();
-
-            //Do mutation under a random probability
-            if (rn.nextInt()%7 < 5) {
-                demo.mutation();
-            }
-
-            //Add fittest offspring to population
-            demo.addFittestOffspring();
-
-            //Calculate new fitness value
-            demo.population.calculateFitness();
-
-            System.out.println("Generation: " + demo.generationCount + " Fittest: " + demo.population.fittest);
-        }
-
-        System.out.println("\nSolution found in generation " + demo.generationCount);
-        System.out.println("Fitness: "+demo.population.getFittest().fitness);
-        System.out.print("Genes: ");
-        for (int i = 0; i < 5; i++) {
-            System.out.print(demo.population.getFittest().genes[i]);
-        }
-
-        System.out.println("");
-
-    }
-
+    
     //Selection
     void selection() {
 
@@ -225,6 +176,55 @@ class Population {
             individuals[i].calcFitness();
         }
         getFittest();
+    }
+    
+    public static void main(String[] args) {
+
+        Random rn = new Random();
+
+        Main demo = new Main ();
+
+        //Initialize population
+        demo.population.initializePopulation(10);
+
+        //Calculate fitness of each individual
+        demo.population.calculateFitness();
+
+        System.out.println("Generation: " + demo.generationCount + " Fittest: " + demo.population.fittest);
+
+        //While population gets an individual with maximum fitness
+        while (demo.population.fittest < 5) {
+            ++demo.generationCount;
+
+            //Do selection
+            demo.selection();
+
+            //Do crossover
+            demo.crossover();
+
+            //Do mutation under a random probability
+            if (rn.nextInt()%7 < 5) {
+                demo.mutation();
+            }
+
+            //Add fittest offspring to population
+            demo.addFittestOffspring();
+
+            //Calculate new fitness value
+            demo.population.calculateFitness();
+
+            System.out.println("Generation: " + demo.generationCount + " Fittest: " + demo.population.fittest);
+        }
+
+        System.out.println("\nSolution found in generation " + demo.generationCount);
+        System.out.println("Fitness: "+demo.population.getFittest().fitness);
+        System.out.print("Genes: ");
+        for (int i = 0; i < 5; i++) {
+            System.out.print(demo.population.getFittest().genes[i]);
+        }
+
+        System.out.println("");
+
     }
 
 }
